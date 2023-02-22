faa0f46b0dddfcac0d97f22dc8f2bc3a0ee54467
raf0nx <rafalpawlowski2011@gmail.com>
2023-02-23 00:58:44 +0100

    Add basic Table Body structure and style header cells

--- src/components/workouts-table/WorkoutsTable.tsx
+++ src/components/workouts-table/WorkoutsTable.tsx
@@ -1,12 +1,23 @@
 import {
+  styled,
   Table,
+  TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
 } from '@suid/material'
+import { tableCellClasses } from '@suid/material/TableCell'
 
 import Card from '~/components/card'
+
+const TableHeaderCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    fontWeight: 600,
+  },
+}))
 
 export default function WorkoutsTable() {
   return (
@@ -15,14 +26,29 @@ export default function WorkoutsTable() {
         <Table stickyHeader>
           <TableHead>
             <TableRow>
-              <TableCell>Workout&nbsp;name</TableCell>
-              <TableCell>Description</TableCell>
-              <TableCell>Workout&nbsp;volume</TableCell>
-              <TableCell>Time</TableCell>
-              <TableCell>Week</TableCell>
-              <TableCell>Date</TableCell>
+              <TableHeaderCell>Workout&nbsp;name</TableHeaderCell>
+              <TableHeaderCell>Description</TableHeaderCell>
+              <TableHeaderCell>Workout&nbsp;volume</TableHeaderCell>
+              <TableHeaderCell>Time</TableHeaderCell>
+              <TableHeaderCell>Week</TableHeaderCell>
+              <TableHeaderCell>Date</TableHeaderCell>
             </TableRow>
           </TableHead>
+          <TableBody>
+            <TableRow
+              hover
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                Push Up Monday
+              </TableCell>
+              <TableCell>low on energy</TableCell>
+              <TableCell>120</TableCell>
+              <TableCell>01:00</TableCell>
+              <TableCell>4</TableCell>
+              <TableCell>01.01.2023</TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </TableContainer>
     </Card>
