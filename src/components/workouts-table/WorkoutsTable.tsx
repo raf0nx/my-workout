ab62443172f703a37292b40b1d24cde3106dc84a
Rafał <69021954+raf0nx@users.noreply.github.com>
2023-02-23 01:36:27 +0100

    feat: Create `WorkoutsTable` component (#3)
    * Add basic `WorkoutsTable` structure and re-export the component
    
    * Add basic Table Body structure and style header cells
    
    * Extract `TableHeaderCell` to the separate component
    
    * Re-organize the columns, align numeric columns to the right
    
    * Add temporary mockedData for `WorkoutsData` and display it
    
    * Extract `WorkoutsTable` headers
    
    * Render `WorkoutsTable` in the home route
    
    * Add padding to all `main`s

---
+++ src/components/workouts-table/WorkoutsTable.tsx
@@ -0,0 +1,63 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@suid/material'
+import { For, Index } from 'solid-js'
+
+import Card from '~/components/card'
+import TableHeaderCell from '~/components/table-header-cell'
+import { workouts } from '~/mockedData'
+
+const WORKOUTS_TABLE_HEADERS = [
+  'Workout name',
+  'Description',
+  'Total reps',
+  'Week',
+  'Date',
+  'Duration',
+] as const
+
+export default function WorkoutsTable() {
+  return (
+    <Card>
+      <TableContainer sx={{ borderRadius: 1 }}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <Index each={WORKOUTS_TABLE_HEADERS}>
+                {(headerValue, i) => (
+                  <TableHeaderCell align={i > 1 ? 'right' : 'left'}>
+                    {headerValue()}
+                  </TableHeaderCell>
+                )}
+              </Index>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <For each={workouts}>
+              {workout => (
+                <TableRow
+                  hover
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {workout.name}
+                  </TableCell>
+                  <TableCell>{workout.description}</TableCell>
+                  <TableCell align="right">{workout.totalReps}</TableCell>
+                  <TableCell align="right">{workout.week}</TableCell>
+                  <TableCell align="right">{workout.date}</TableCell>
+                  <TableCell align="right">{workout.duration}</TableCell>
+                </TableRow>
+              )}
+            </For>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Card>
+  )
+}
