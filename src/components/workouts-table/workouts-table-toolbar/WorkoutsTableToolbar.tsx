af76f5defabd6a58671cb0928e31afa9d410f1f4
raf0nx <rafalpawlowski2011@gmail.com>
2023-02-25 02:50:20 +0100

    Handle opening and closing dialog logic

--- src/components/workouts-table/workouts-table-toolbar/WorkoutsTableToolbar.tsx
+++ src/components/workouts-table/workouts-table-toolbar/WorkoutsTableToolbar.tsx
@@ -1,15 +1,28 @@
 import { AddCircle } from '@suid/icons-material'
 import { IconButton, Toolbar, Typography } from '@suid/material'
+import { createSignal } from 'solid-js'
+
+import WorkoutsTableDialog from '~/components/workouts-table/workouts-table-dialog'
 
 export default function WorkoutsTableToolbar() {
+  const [isDialogOpen, setIsDialogOpen] = createSignal(false)
+
   return (
     <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
       <Typography variant="h6" component="h1" sx={{ pl: 1 }}>
         Workouts
       </Typography>
-      <IconButton color="secondary" aria-label="add new workout">
+      <IconButton
+        onClick={[setIsDialogOpen, true]}
+        color="secondary"
+        aria-label="add new workout"
+      >
         <AddCircle />
       </IconButton>
+      <WorkoutsTableDialog
+        isOpen={isDialogOpen()}
+        onClose={[setIsDialogOpen, false]}
+      />
     </Toolbar>
   )
 }
