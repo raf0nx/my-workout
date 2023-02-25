8ba78854286b5b5e4baf972bd44287bcc77584b5
raf0nx <rafalpawlowski2011@gmail.com>
2023-02-25 01:41:49 +0100

    Change toolbar title to just `Workouts`

--- src/components/workouts-table/workouts-table-toolbar/WorkoutsTableToolbar.tsx
+++ src/components/workouts-table/workouts-table-toolbar/WorkoutsTableToolbar.tsx
@@ -5,7 +5,7 @@ export default function WorkoutsTableToolbar() {
   return (
     <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
       <Typography variant="h6" component="h1" sx={{ pl: 1 }}>
-        Workouts list
+        Workouts
       </Typography>
       <IconButton color="secondary" aria-label="add new workout">
         <AddCircle />
