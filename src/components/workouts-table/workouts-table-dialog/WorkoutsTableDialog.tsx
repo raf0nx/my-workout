2897dad81062a25a49c90705ca4e76a5e582deb8
Rafał <69021954+raf0nx@users.noreply.github.com>
2023-03-01 00:20:34 +0100

    refactor: Refactor `WorkoutsTableDialog` (#10)
    * Extract `WorkoutsTableDialogBar` component
    
    * Extract `WorkoutsTableDialogContent` component
    
    * Leverage recently extracted components
    
    * v0.5.3

--- src/components/workouts-table/workouts-table-dialog/WorkoutsTableDialog.tsx
+++ src/components/workouts-table/workouts-table-dialog/WorkoutsTableDialog.tsx
@@ -1,25 +1,16 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  Dialog,
-  Grid,
-  IconButton,
-  TextField,
-  Toolbar,
-  Typography,
-} from '@suid/material'
-import { Close } from '@suid/icons-material'
-import { ChangeEvent } from '@suid/types'
+import { Dialog } from '@suid/material'
+import type { ChangeEvent } from '@suid/types'
 import { createStore, produce } from 'solid-js/store'
 
 import TransitionSlideUp from '~/utils/transition-slide-up'
-import { WorkoutProps } from '~/components/workouts-table/workouts-table-types'
+import type { WorkoutProps } from '~/components/workouts-table/workouts-table-types'
 
 import {
   workoutDetailsInitialState,
   type WorkoutsTableDialogProps,
 } from './workouts-table-dialog-types'
+import WorkoutsTableDialogBar from './workouts-table-dialog-bar'
+import WorkoutsTableDialogContent from './workouts-table-dialog-content'
 
 // TODO: Implement Dialog's accessibility
 export default function WorkoutsTableDialog(props: WorkoutsTableDialogProps) {
@@ -57,101 +48,8 @@ export default function WorkoutsTableDialog(props: WorkoutsTableDialogProps) {
       onClose={props.onClose}
       aria-labelledby="workouts-table-dialog-title"
     >
-      <AppBar sx={{ position: 'relative' }}>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            onClick={props.onClose}
-            aria-label="close"
-          >
-            <Close />
-          </IconButton>
-          <Typography
-            sx={{
-              ml: 2,
-              flex: 1,
-            }}
-            variant="h6"
-            component="h2"
-            id="workouts-table-dialog-title"
-          >
-            New&nbsp;Workout
-          </Typography>
-          <Button autofocus color="inherit" onClick={handleSave}>
-            Save
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Box p={3} component="form" noValidate autocomplete="off">
-        <Grid container spacing={4}>
-          <Grid item xs={12} sm={4}>
-            <TextField
-              label="Workout name"
-              name="name"
-              margin="normal"
-              placeholder="Push Pull Monday"
-              fullWidth
-              onChange={handleInputChange}
-            />
-            <TextField
-              label="Description"
-              name="description"
-              margin="normal"
-              placeholder="e.g. Today the weather was beautiful so the quality of the training was excellent."
-              fullWidth
-              onChange={handleInputChange}
-            />
-            <Box sx={{ display: 'flex', gap: 3 }}>
-              <TextField
-                label="Total reps"
-                name="totalReps"
-                type="number"
-                margin="normal"
-                placeholder="90"
-                sx={{ flex: 1 }}
-                onChange={handleInputChange}
-              />
-              <TextField
-                label="Week"
-                name="week"
-                type="number"
-                margin="normal"
-                placeholder="4"
-                sx={{ flex: 1 }}
-                onChange={handleInputChange}
-              />
-            </Box>
-            <TextField
-              label="Date"
-              name="date"
-              margin="normal"
-              placeholder="DD.MM.YYYY"
-              fullWidth
-              onChange={handleInputChange}
-            />
-            <TextField
-              label="Duration"
-              name="duration"
-              type="number"
-              margin="normal"
-              placeholder="60"
-              fullWidth
-              InputProps={{
-                endAdornment: (
-                  <Typography variant="body2" sx={{ ml: 1 }}>
-                    mins
-                  </Typography>
-                ),
-              }}
-              onChange={handleInputChange}
-            />
-          </Grid>
-          <Grid item xs={12} sm={8}>
-            {/* TODO: Exercises table */}
-          </Grid>
-        </Grid>
-      </Box>
+      <WorkoutsTableDialogBar onClose={props.onClose} onSave={handleSave} />
+      <WorkoutsTableDialogContent onInputChange={handleInputChange} />
     </Dialog>
   )
 }
