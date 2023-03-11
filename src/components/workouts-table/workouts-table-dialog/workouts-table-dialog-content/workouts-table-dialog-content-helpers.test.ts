353c47c56df1304bf9787b25202822d730951820
raf0nx <rafalpawlowski2011@gmail.com>
2023-03-11 16:35:27 +0100

    Adjust tests for recent `workouts-table-dialog-content-helpers` update

--- src/components/workouts-table/workouts-table-dialog/workouts-table-dialog-content/workouts-table-dialog-content-helpers.test.ts
+++ src/components/workouts-table/workouts-table-dialog/workouts-table-dialog-content/workouts-table-dialog-content-helpers.test.ts
@@ -1,11 +1,14 @@
 import { describe, it } from 'vitest'
 
-import { getInputProps } from './workouts-table-dialog-content-helpers'
+import {
+  getInputProps,
+  getInputStyle,
+} from './workouts-table-dialog-content-helpers'
 
 describe('getInputProps', () => {
   it('should return proper input properties for readonly input', () => {
     // Given
-    const expected = { readOnly: true, disableUnderline: true }
+    const expected = { readOnly: true, disabled: true }
 
     // When
     const actual = getInputProps(true)
@@ -16,12 +19,30 @@ describe('getInputProps', () => {
 
   it('should NOT return any input properties for non-readonly input', () => {
     // Given
-    const expected = {}
+    const expected = { readOnly: false, disabled: false }
 
     // When
     const actual = getInputProps(false)
 
     // Then
     expect(actual).toEqual(expected)
+  })
+})
+
+describe('getInputStyle', () => {
+  it('should return input styles for readonly input', () => {
+    // When
+    const actual = getInputStyle(true)
+
+    // Then
+    expect(actual).toBeTruthy()
+  })
+
+  it('should NOT return any additional input styles for non-readonly input', () => {
+    // When
+    const actual = getInputStyle(false)
+
+    // Then
+    expect(actual).toBeUndefined()
   })
 })
