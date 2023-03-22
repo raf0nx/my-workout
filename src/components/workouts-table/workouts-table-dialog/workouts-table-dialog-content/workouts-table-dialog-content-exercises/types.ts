1adad949792854b4234bff1b94a11255d0f445f0
raf0nx <rafalpawlowski2011@gmail.com>
2023-03-23 00:53:57 +0100

    Add `state` prop to `WorkoutsTableDialogContentExercises` and pass it

--- src/components/workouts-table/workouts-table-dialog/workouts-table-dialog-content/workouts-table-dialog-content-exercises/types.ts
+++ src/components/workouts-table/workouts-table-dialog/workouts-table-dialog-content/workouts-table-dialog-content-exercises/types.ts
@@ -1,9 +1,11 @@
 import type { SetStoreFunction } from 'solid-js/store'
 
 import type { Exercises, Workout } from '~/components/workouts-table/types'
+import type { WorkoutsTableDialogState } from '~/components/workouts-table/workouts-table-dialog/types'
 
 export interface WorkoutsTableDialogContentExercisesProps {
   exercises: Exercises
+  state: WorkoutsTableDialogState
   setWorkoutDetails: SetStoreFunction<Workout>
 }
 
