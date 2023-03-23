2d3fa0ce2d4c29b2d2ed6d68054947614340dc80
raf0nx <rafalpawlowski2011@gmail.com>
2023-03-23 01:35:12 +0100

    Conditionally show `TextField` instead of `ExercisesSelect` component when it's in `readOnly` state

--- src/components/exercises-select/ExercisesSelect.tsx
+++ src/components/exercises-select/ExercisesSelect.tsx
@@ -1,6 +1,11 @@
-import { FormControl, MenuItem, Select } from '@suid/material'
+import { FormControl, MenuItem, Select, TextField } from '@suid/material'
 import type { SelectChangeEvent } from '@suid/material/Select'
 import { Index, Show } from 'solid-js'
+
+import {
+  getInputProps,
+  getInputStyle,
+} from '~/components/workouts-table/workouts-table-dialog/workouts-table-dialog-content/workouts-table-dialog-content-helpers'
 
 import {
   type ExercisesSelectProps,
@@ -17,23 +22,42 @@ export default function ExercisesSelect(props: ExercisesSelectProps) {
   }
 
   return (
-    <FormControl variant="standard" size="small" sx={{ width: '100%' }}>
-      <Select
-        value={props.selectedExercise}
-        name={props.name}
-        displayEmpty
-        onChange={handleChange}
-        inputProps={{ 'aria-label': props.ariaLabel }}
-      >
-        <Show when={!props.selectedExercise}>
-          <MenuItem value="">
-            <em>Select&nbsp;exercise</em>
-          </MenuItem>
-        </Show>
-        <Index each={AVAILABLE_EXERCISES}>
-          {exercise => <MenuItem value={exercise()}>{exercise()}</MenuItem>}
-        </Index>
-      </Select>
-    </FormControl>
+    <Show
+      when={!props.isReadOnly}
+      fallback={
+        <TextField
+          classes={{
+            root: getInputStyle(true),
+          }}
+          sx={{ mb: 0.5 }}
+          name={props.name}
+          fullWidth
+          inputProps={{ 'aria-label': props.ariaLabel, ...getInputProps(true) }}
+          defaultValue={props.selectedExercise}
+          variant="standard"
+        />
+      }
+    >
+      <FormControl variant="standard" size="small" sx={{ width: '100%' }}>
+        <Select
+          value={props.selectedExercise}
+          name={props.name}
+          displayEmpty
+          onChange={handleChange}
+          inputProps={{
+            'aria-label': props.ariaLabel,
+          }}
+        >
+          <Show when={!props.selectedExercise}>
+            <MenuItem value="">
+              <em>Select&nbsp;exercise</em>
+            </MenuItem>
+          </Show>
+          <Index each={AVAILABLE_EXERCISES}>
+            {exercise => <MenuItem value={exercise()}>{exercise()}</MenuItem>}
+          </Index>
+        </Select>
+      </FormControl>
+    </Show>
   )
 }
