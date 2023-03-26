f4f830a3b1382955a585a72a7c6e2ae333c48461
raf0nx <rafalpawlowski2011@gmail.com>
2023-03-26 16:34:45 +0200

    Add `getWorkouts` api endpoint with data transformation logic

---
+++ src/api/workouts.ts
@@ -0,0 +1,37 @@
+import {
+  collection,
+  getDocs,
+  type DocumentData,
+  type QuerySnapshot,
+} from 'firebase/firestore'
+
+import { db } from '~/config/firebase-config'
+import { keys } from '~/utils/utils'
+import type { Exercises, Workout } from '~/components/workouts-table/types'
+
+const workoutsCollection = collection(db, 'workouts')
+
+export const getWorkouts = async (): Promise<Workout[]> => {
+  const data = await getDocs(workoutsCollection)
+
+  return transformDocsToWorkoutObjects(data)
+}
+
+const transformDocsToWorkoutObjects = (
+  data: QuerySnapshot<DocumentData>
+): Workout[] => {
+  return data.docs.map(doc => {
+    const docData = doc.data() as Workout
+
+    return {
+      ...docData,
+      id: doc.id,
+      exercises: sortWorkoutExercises(docData.exercises),
+    }
+  })
+}
+
+const sortWorkoutExercises = (exercises: Exercises) =>
+  keys(exercises)
+    .sort()
+    .reduce<Exercises>((acc, curr) => ({ ...acc, [curr]: exercises[curr] }), {})
