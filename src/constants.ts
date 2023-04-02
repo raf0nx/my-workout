f9d9d8d51b48d42c8bd5660ca2f26771b996e7ce
raf0nx <rafalpawlowski2011@gmail.com>
2023-04-03 01:16:33 +0200

    Add `constants` file

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const DEVELOPMENT_MODE = 'development'
+export const TEST_MODE = 'test'
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,10 @@
+import { DEVELOPMENT_MODE, TEST_MODE } from '~/constants'
+
 export const removeTrailingNonDigits = (str: string): string =>
   str.replace(/^\D+/g, '')
 
 export const keys = Object.keys as <T extends object>(obj: T) => Array<keyof T>
 
-export const isDevMode = () => import.meta.env.MODE === 'development'
+export const isDevMode = () => import.meta.env.MODE === DEVELOPMENT_MODE
 
-export const isTestMode = () => import.meta.env.MODE === 'test'
+export const isTestMode = () => import.meta.env.MODE === TEST_MODE
