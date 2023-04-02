e61f5eaeaac06279c724c311d1e4089ea4e22859
Rafał <69021954+raf0nx@users.noreply.github.com>
2023-04-03 01:21:18 +0200

    feat: Add `firestore` emulator (#26)
    * Init firebase project and add firestore emulator
    
    * Add host property to firestore emulator
    
    * Update .gitignore
    
    * Add `isDevMode` and `isTestMode` utilities
    
    * Add `constants` file
    
    * Connect to firestore emulator when app is either in development or test mode
    
    * v0.9.1

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,10 @@
+import { DEVELOPMENT_MODE, TEST_MODE } from '~/constants'
+
 export const removeTrailingNonDigits = (str: string): string =>
   str.replace(/^\D+/g, '')
 
 export const keys = Object.keys as <T extends object>(obj: T) => Array<keyof T>
+
+export const isDevMode = () => import.meta.env.MODE === DEVELOPMENT_MODE
+
+export const isTestMode = () => import.meta.env.MODE === TEST_MODE
