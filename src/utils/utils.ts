20b2ae04adb3106a865745d57c491fce3dc74a50
raf0nx <rafalpawlowski2011@gmail.com>
2023-04-03 01:15:12 +0200

    Add `isDevMode` and `isTestMode` utilities

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,3 +2,7 @@ export const removeTrailingNonDigits = (str: string): string =>
   str.replace(/^\D+/g, '')
 
 export const keys = Object.keys as <T extends object>(obj: T) => Array<keyof T>
+
+export const isDevMode = () => import.meta.env.MODE === 'development'
+
+export const isTestMode = () => import.meta.env.MODE === 'test'
