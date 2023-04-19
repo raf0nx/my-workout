55d69b7b45f4f1cf08f71a224d381986ea02af5f
raf0nx <rafalpawlowski2011@gmail.com>
2023-04-20 00:35:12 +0200

    Prefill Workout Week in `getWorkoutDetailsInitialState` helper

--- src/components/workouts-table/workouts-table-dialog/workouts-table-dialog-helper.ts
+++ src/components/workouts-table/workouts-table-dialog/workouts-table-dialog-helper.ts
@@ -1,11 +1,15 @@
 import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+
 import type { Workout, WorkoutDateFormat } from '../types'
+
+dayjs.extend(weekOfYear)
 
 export const getWorkoutDetailsInitialState = (): Workout => ({
   name: '',
   description: '',
   totalReps: '',
-  week: '',
+  week: getCurrentWeekOfYear(),
   date: getCurrentDateInWorkoutDateFormat(),
   duration: '',
   exercises: { exercise1: { name: '', sets: [0] } },
@@ -13,3 +17,5 @@ export const getWorkoutDetailsInitialState = (): Workout => ({
 
 export const getCurrentDateInWorkoutDateFormat = (): WorkoutDateFormat =>
   dayjs().format('DD.MM.YYYY') as WorkoutDateFormat
+
+export const getCurrentWeekOfYear = () => dayjs().week().toString()
