e8abfb8c4149f393a3fef0e93fe7d3d356434be8
raf0nx <rafalpawlowski2011@gmail.com>
2023-04-25 23:22:13 +0200

    Remove code coverage check from insignificant files/lines

--- src/components/workouts-table/workouts-table-helper.ts
+++ src/components/workouts-table/workouts-table-helper.ts
@@ -1,4 +1,5 @@
 import { QUARTER_HOUR } from '~/constants'
 import { isTestMode } from '~/utils/utils'
 
+/* c8 ignore next */
 export const getWorkoutsQueryStaleTime = () => (isTestMode() ? 0 : QUARTER_HOUR)
