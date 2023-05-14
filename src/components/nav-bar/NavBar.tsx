40e4894f492e3cc2b6a750e606abac9dc8a68504
raf0nx <rafalpawlowski2011@gmail.com>
2023-05-14 16:24:12 +0200

    Implement `NavBar` content

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,16 +1,45 @@
-import { Drawer, Toolbar } from '@suid/material'
+import { Dashboard, FitnessCenter } from '@suid/icons-material'
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from '@suid/material'
+import { Index, Show } from 'solid-js'
+
+import { NAV_WIDTH } from '~/constants'
 
 export default function NavBar() {
+  const NAV_ITEMS = ['Dashboard', 'Workouts']
+
   return (
     <Drawer
       variant="permanent"
       sx={{
-        width: 240,
-        [`& .MuiDrawer-paper`]: { width: 240 },
+        width: NAV_WIDTH,
+        [`& .MuiDrawer-paper`]: { width: NAV_WIDTH },
       }}
     >
       <Toolbar />
-      Test
+      <List>
+        <Index each={NAV_ITEMS}>
+          {(text, idx) => (
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Show when={idx === 0} fallback={<FitnessCenter />}>
+                    <Dashboard />
+                  </Show>
+                </ListItemIcon>
+                <ListItemText primary={text()} />
+              </ListItemButton>
+            </ListItem>
+          )}
+        </Index>
+      </List>
     </Drawer>
   )
 }
--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,8 @@ export const WORKOUTS_DOC_ID = 'workouts'
 export const DOCUMENT_POSITION_FOLLOWING = 4
 export const QUARTER_HOUR = 900_000
 
+export const NAV_WIDTH = 200
+
 export enum OrderDirection {
   ASCENDING = 'asc',
   DESCENDING = 'desc',
