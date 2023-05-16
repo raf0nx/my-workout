2fc719acc2faed0f938fb5610ca9632000ac1975
raf0nx <rafalpawlowski2011@gmail.com>
2023-05-16 23:36:43 +0200

    Hide hamburger icon when not in mobile view

--- src/components/app-bar/AppBar.tsx
+++ src/components/app-bar/AppBar.tsx
@@ -6,22 +6,28 @@ import {
   useTheme,
 } from '@suid/material'
 import MenuIcon from '@suid/icons-material/Menu'
+import { Show } from 'solid-js'
+
+import { useUi } from '~/contexts/ui'
 
 export default function AppBar() {
   const theme = useTheme()
+  const { isMobileDesign } = useUi()
 
   return (
     <SuidAppBar position="sticky" sx={{ zIndex: theme.zIndex.drawer + 1 }}>
       <Toolbar>
-        <IconButton
-          size="large"
-          color="inherit"
-          edge="start"
-          aria-label="open navigation drawer"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
+        <Show when={isMobileDesign()}>
+          <IconButton
+            size="large"
+            color="inherit"
+            edge="start"
+            aria-label="open navigation drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Show>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Dashboard
         </Typography>
