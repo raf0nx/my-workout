0ab03854ae08a89924c0c3c53f55bbdf2b8dc275
raf0nx <rafalpawlowski2011@gmail.com>
2023-05-26 01:06:40 +0200

    Read total workouts amount from database and display it in KPI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,31 @@
 import { SportsGymnastics } from '@suid/icons-material'
 import { Box, Grid, Typography, useTheme } from '@suid/material'
+import { createQuery, type CreateQueryResult } from '@tanstack/solid-query'
 
+import { getWorkouts } from '~/api/workouts'
 import { Card } from '~/components/card'
+import type { Workout } from '~/components/workouts-table/types'
+import {
+  getGetWorkoutsErrorSnackbarProps,
+  getWorkoutsQueryStaleTime,
+} from '~/components/workouts-table/workouts-table-helpers'
+import { WORKOUTS_DOC_ID } from '~/constants'
+import { useSnackbar } from '~/contexts/snackbar'
 
 export default function Dashboard() {
+  const { showSnackbar } = useSnackbar()
   const theme = useTheme()
+
+  const workoutsQuery: CreateQueryResult<Workout[]> = createQuery(
+    () => [WORKOUTS_DOC_ID],
+    getWorkouts,
+    {
+      staleTime: getWorkoutsQueryStaleTime(),
+      onError: () => showSnackbar(getGetWorkoutsErrorSnackbarProps()),
+    }
+  )
+
+  const totalWorkoutsAmount = () => workoutsQuery.data?.length || 0
 
   return (
     <main>
@@ -39,7 +60,7 @@ export default function Dashboard() {
                 }}
               >
                 <Typography variant="h6" component="h2" fontWeight={700}>
-                  123
+                  {totalWorkoutsAmount()}
                 </Typography>
                 <Typography variant="body2" sx={{ opacity: 0.6 }}>
                   Total Workouts
