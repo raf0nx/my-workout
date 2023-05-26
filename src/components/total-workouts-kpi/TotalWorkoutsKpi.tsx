4425a90f9ecfa8a7cd1bb64e882fd562a386ee1a
raf0nx <rafalpawlowski2011@gmail.com>
2023-05-26 02:07:54 +0200

    Reuse `Kpi` component in `TotalWorkoutsKpi`

--- src/components/total-workouts-kpi/TotalWorkoutsKpi.tsx
+++ src/components/total-workouts-kpi/TotalWorkoutsKpi.tsx
@@ -1,7 +1,7 @@
 import { SportsGymnastics } from '@suid/icons-material'
-import { Box, Typography, useTheme } from '@suid/material'
+import { useTheme } from '@suid/material'
 
-import { Card } from '~/components/card'
+import { Kpi } from '~/components/kpi'
 
 import type { TotalWorkoutsKpiProps } from './types'
 
@@ -9,42 +9,11 @@ export default function TotalWorkoutsKpi(props: TotalWorkoutsKpiProps) {
   const theme = useTheme()
 
   return (
-    <Card>
-      <Box sx={{ display: 'flex', gap: 2 }}>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: `${theme.palette.secondary.main}1A`,
-            borderRadius: '50%',
-            height: '4rem',
-            width: '4rem',
-          }}
-        >
-          <SportsGymnastics
-            sx={{
-              fontSize: '2rem',
-              color: theme.palette.secondary.main,
-            }}
-          />
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            flex: 1,
-            justifyContent: 'center',
-          }}
-        >
-          <Typography variant="h6" component="h2" fontWeight={700}>
-            {props.totalWorkoutsAmount}
-          </Typography>
-          <Typography variant="body2" sx={{ opacity: 0.6 }}>
-            Total Workouts
-          </Typography>
-        </Box>
-      </Box>
-    </Card>
+    <Kpi
+      value={props.totalWorkoutsAmount}
+      description="Total Workouts"
+      color={theme.palette.secondary.main}
+      icon={SportsGymnastics}
+    />
   )
 }
