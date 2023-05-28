fb9eec2dbd318b317ff35e70621396bb3f33e5f6
raf0nx <rafalpawlowski2011@gmail.com>
2023-05-28 18:10:47 +0200

    Add more info button to the `WeightKpi`

--- src/components/weight-kpi/WeightKpi.tsx
+++ src/components/weight-kpi/WeightKpi.tsx
@@ -1,5 +1,5 @@
-import { MonitorWeight } from '@suid/icons-material'
-import { useTheme } from '@suid/material'
+import { MonitorWeight, MoreHoriz } from '@suid/icons-material'
+import { Box, IconButton, useTheme } from '@suid/material'
 
 import { Kpi } from '~/components/kpi'
 
@@ -7,12 +7,21 @@ export default function WeightKpi() {
   const theme = useTheme()
 
   return (
-    <Kpi
-      value={80.4}
-      changeValue={'-1.4%'}
-      description="Current weight"
-      color={theme.palette.info.main}
-      icon={MonitorWeight}
-    />
+    <Box sx={{ position: 'relative' }}>
+      <Kpi
+        value={80.4}
+        changeValue={'-1.4%'}
+        description="Current weight"
+        color={theme.palette.info.main}
+        icon={MonitorWeight}
+      />
+      <IconButton
+        size="small"
+        aria-label="open weight information modal"
+        sx={{ position: 'absolute', top: 4, right: 4 }}
+      >
+        <MoreHoriz />
+      </IconButton>
+    </Box>
   )
 }
