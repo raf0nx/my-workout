e0e9927267076d9dceb8edd95dd0b028a20c8522
raf0nx <rafalpawlowski2011@gmail.com>
2023-05-28 17:01:07 +0200

    Change `0%` `Kpi` change value color

--- src/components/kpi/kpi-helpers.ts
+++ src/components/kpi/kpi-helpers.ts
@@ -4,7 +4,7 @@ export const getChangeValueColor = (changeValue: string) => {
   const { palette } = useTheme()
   const numericChangeValue = parseFloat(changeValue)
 
-  if (!numericChangeValue) return palette.grey[700]
+  if (!numericChangeValue) return palette.grey[600]
 
   return numericChangeValue > 0 ? palette.success.main : palette.error.main
 }
