bd880cfed869f649e4cb957d9e1c6dac79cfa83e
raf0nx <rafalpawlowski2011@gmail.com>
2023-06-03 20:52:33 +0200

    Implement a better `sortWeightsInfoByNewestFirst` sorting function

--- src/api/user-data/user-data-helpers.ts
+++ src/api/user-data/user-data-helpers.ts
@@ -2,15 +2,25 @@ import { arrayUnion } from 'firebase/firestore'
 import type { QueryClient } from '@tanstack/solid-query'
 
 import { USER_DATA_WEIGHT_QUERY_KEY } from '~/constants'
-import { getCurrentDateInDDMMYYYYFormat } from '~/utils/utils'
+import {
+  getCurrentDateInDDMMYYYYFormat,
+  parseDateInDDMMYYYYFormat,
+} from '~/utils/utils'
 import type { WeightInfo } from '~/components/weight-kpi/weight-kpi-dialog/weight-kpi-dialog-content/types'
 
 export const createUserWeightData = (weight: string) => ({
   weightsInfo: arrayUnion({ weight, date: getCurrentDateInDDMMYYYYFormat() }),
 })
 
-export const sortWeightsInfoByNewestFirst = (weightsInfo: WeightInfo[]) =>
-  weightsInfo.reverse()
+export const sortWeightsInfoByNewestFirst = (
+  weightsInfo: WeightInfo[]
+): WeightInfo[] =>
+  [...weightsInfo].sort((currWeightInfo, nextWeightInfo) => {
+    const currWeightInfoDate = parseDateInDDMMYYYYFormat(currWeightInfo.date)
+    const nextWeightInfoDate = parseDateInDDMMYYYYFormat(nextWeightInfo.date)
+
+    return nextWeightInfoDate.getTime() - currWeightInfoDate.getTime()
+  })
 
 export const invalidateUserWeightQuery = (queryClient: QueryClient) => {
   queryClient.invalidateQueries([USER_DATA_WEIGHT_QUERY_KEY])
