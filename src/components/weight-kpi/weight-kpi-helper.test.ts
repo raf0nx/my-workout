8b97c2836d5a6590af92248a09a5bc5ca441bb66
raf0nx <rafalpawlowski2011@gmail.com>
2023-06-05 02:20:14 +0200

    Add unit tests for `getWeightKpiChangeValue`

--- src/components/weight-kpi/weight-kpi-helper.test.ts
+++ src/components/weight-kpi/weight-kpi-helper.test.ts
@@ -3,7 +3,8 @@ import { describe, expect, it } from 'vitest'
 import { weightsInfo } from '~/mocked-data'
 import { KPI_NO_VALUE_INDICATOR } from '~/constants'
 
-import { getWeightKpiValue } from './weight-kpi-helper'
+import { getWeightKpiChangeValue, getWeightKpiValue } from './weight-kpi-helper'
+import type { WeightInfo } from './weight-kpi-dialog/weight-kpi-dialog-content/types'
 
 describe('getWeightKpiValue', () => {
   it('should return the weight value of the latest entry if available', () => {
@@ -28,5 +29,59 @@ describe('getWeightKpiValue', () => {
 
     // Then
     expect(result).toBe(KPI_NO_VALUE_INDICATOR)
+  })
+})
+
+describe('getWeightKpiChangeValue', () => {
+  it('should return undefined if current weight is undefined', () => {
+    // Given
+    const weightsInfo: WeightInfo[] = []
+
+    // When
+    const actual = getWeightKpiChangeValue(weightsInfo)
+
+    // Then
+    expect(actual).toBeUndefined()
+  })
+
+  it('should return "--" if previous weight is undefined', () => {
+    // Given
+    const weightsInfo: WeightInfo[] = [{ weight: 75, date: '12.05.2023' }]
+
+    // When
+    const actual = getWeightKpiChangeValue(weightsInfo)
+
+    // Then
+    expect(actual).toBe('--')
+  })
+
+  it('should return the calculated weight change percentage as a formatted string', () => {
+    // Given
+    const weightsInfo: WeightInfo[] = [
+      { weight: 75, date: '14.05.2023' },
+      { weight: 80, date: '12.05.2023' },
+    ]
+    const expected = '-6.25%'
+
+    // When
+    const actual = getWeightKpiChangeValue(weightsInfo)
+
+    // Then
+    expect(actual).toBe(expected)
+  })
+
+  it('should return the calculated weight change percentage with negative sign as a formatted string', () => {
+    // Given
+    const weightsInfo: WeightInfo[] = [
+      { weight: 80, date: '14.05.2023' },
+      { weight: 75, date: '12.05.2023' },
+    ]
+    const expected = '+6.67%'
+
+    // When
+    const actual = getWeightKpiChangeValue(weightsInfo)
+
+    // Then
+    expect(actual).toBe(expected)
   })
 })
