605aea0b85c04709ae439b10ea1be5aeb203b532
raf0nx <rafalpawlowski2011@gmail.com>
2023-06-07 23:56:23 +0200

    Use `WeightsTableSpinner` as an fallback when weightsInfo is being fetched

--- src/components/weight-kpi/weight-kpi-dialog/weight-kpi-dialog-content/WeightsTable.tsx
+++ src/components/weight-kpi/weight-kpi-dialog/weight-kpi-dialog-content/WeightsTable.tsx
@@ -6,43 +6,46 @@ import {
   TableHead,
   TableRow,
 } from '@suid/material'
-import { For } from 'solid-js'
+import { For, Suspense } from 'solid-js'
 
 import { TableHeaderCell } from '~/components/table-header-cell'
 
 import type { WeightsTableProps } from './types'
+import { WeightsTableSpinner } from './WeightsTableSpinner'
 
 export function WeightsTable(props: WeightsTableProps) {
   return (
     <TableContainer
       sx={{ mt: 4, borderRadius: 1, maxHeight: 'calc(100vh - 326.5px)' }}
     >
-      <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableHeaderCell>Weight</TableHeaderCell>
-            <TableHeaderCell>Date</TableHeaderCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <For each={props.weightsInfo}>
-            {weightInfo => (
-              <>
-                <TableRow
-                  sx={{
-                    '&:last-child td, &:last-child th': { border: 0 },
-                  }}
-                >
-                  <TableCell component="th" scope="row">
-                    {weightInfo.weight}
-                  </TableCell>
-                  <TableCell>{weightInfo.date}</TableCell>
-                </TableRow>
-              </>
-            )}
-          </For>
-        </TableBody>
-      </Table>
+      <Suspense fallback={<WeightsTableSpinner />}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell>Weight</TableHeaderCell>
+              <TableHeaderCell>Date</TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <For each={props.weightsInfo}>
+              {weightInfo => (
+                <>
+                  <TableRow
+                    sx={{
+                      '&:last-child td, &:last-child th': { border: 0 },
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {weightInfo.weight}
+                    </TableCell>
+                    <TableCell>{weightInfo.date}</TableCell>
+                  </TableRow>
+                </>
+              )}
+            </For>
+          </TableBody>
+        </Table>
+      </Suspense>
     </TableContainer>
   )
 }
