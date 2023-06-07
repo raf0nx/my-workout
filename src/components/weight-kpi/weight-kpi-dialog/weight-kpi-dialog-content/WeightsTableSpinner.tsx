a08812253365732eda0e284b1b1d5036a0c30a58
raf0nx <rafalpawlowski2011@gmail.com>
2023-06-07 23:55:59 +0200

    Add `WeightsTableSpinner` component

---
+++ src/components/weight-kpi/weight-kpi-dialog/weight-kpi-dialog-content/WeightsTableSpinner.tsx
@@ -0,0 +1,17 @@
+import { Box, CircularProgress, Typography } from '@suid/material'
+
+export function WeightsTableSpinner() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 1,
+      }}
+    >
+      <CircularProgress size={32} />
+      <Typography textContent={'Loading weights history'} variant="caption" />
+    </Box>
+  )
+}
