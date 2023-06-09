cacb0de615469197ca7c0efd9045d195ec3a74e9
raf0nx <rafalpawlowski2011@gmail.com>
2023-06-09 19:25:59 +0200

    Add tests for `WeightKpiDialog`

---
+++ src/utils/test-utils/weight-kpi-dialog.ts
@@ -0,0 +1,14 @@
+/* c8 ignore start */
+import { screen } from '@solidjs/testing-library'
+import userEvent from '@testing-library/user-event'
+
+// Getters
+export const getWeightKpiDialogHeader = () =>
+  screen.getByText(/your weight history/i)
+
+export const getCloseBtn = () => screen.getByText(/close/i)
+
+// Actions
+export const clickCloseBtn = async () => {
+  await userEvent.click(getCloseBtn())
+}
